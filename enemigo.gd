a7fcad6c866b5f12f270d9fbb2cdda3086ef5eca
Nahuel Lautaro Jimenez <nahuellautaro.jimenez@estudiantes.unahur.edu.ar>
2024-10-29 16:32:16 -0300

    Agregue el patrullaje de los esqueletos

--- enemigo.gd
+++ enemigo.gd
@@ -1,29 +1,67 @@
 extends CharacterBody2D
-
+#variables para guardar los nodos
 var jugador = null
 var rayCast
+var recorrido
+
+#variables para movimiento
+var velocidad_movimiento = 70
+var direccion = 1
+const gravity = 980
+
+#posicion inicial, para que no se cambie al iniciar la escena y comenzar a patrullar
+var posicion_inicial 
+
+#variables de control
+var esta_caminando_a_derecha = true
+var esta_atacando = false
 
 func _ready():
 	rayCast = $RayCast2D
-	#get parent lo que hace es buscar el nodo entre sus hermanos, es decir entre los nodos que son de su misma jerarquia
+	recorrido = get_node("Camino/Patrullaje")
 	jugador = get_parent().get_node("Personaje") 
-	#si hay un jugador, es decir la var jugador es diferente de null, hace un print diciendo que hay un jugador
+	posicion_inicial = position
 	if jugador:
 		print("Hay un jugador")
 
 func _process(delta):
+	animaciones()
+	patrullar(delta)
+	gravedad(delta)
 	voltear()
 	golpear()
 
 func voltear():
-	if jugador:
-		if jugador.global_position.x < global_position.x:
-			scale.x = -1  # Voltear hacia la izquierda
-		else:
-			scale.x = 1   # Voltear hacia la derecha
+	if esta_caminando_a_derecha:
+		scale.x = 1  # Voltear hacia la izquierda
+	else:
+		scale.x = -1   # Voltear hacia la derecha
+
+
+func gravedad(delta):
+	velocity.y +=  gravity * delta
+
+func animaciones():
+	if esta_atacando:
+		$AnimatedSprite2D.play("attack")
+	else:
+		$AnimatedSprite2D.play("walk")
+
 
 func golpear():
 	var colision = rayCast.get_collider()
-	if rayCast.is_colliding():
+	if rayCast.is_colliding() and not esta_atacando:
 		print("El esqueleto esta colisionando con: ", colision)
-		$AnimatedSprite2D.play("attack")
+		esta_atacando = true 
+		velocity.x = 0
+		await get_tree().create_timer(1).timeout
+		esta_atacando = false
+
+
+func patrullar(delta):
+	if not rayCast.is_colliding():
+		recorrido.progress += delta * velocidad_movimiento * direccion
+		if recorrido.progress_ratio == 0 or recorrido.progress_ratio == 1:
+			direccion *= -1
+			esta_caminando_a_derecha = not esta_caminando_a_derecha
+		position = posicion_inicial + recorrido.position
